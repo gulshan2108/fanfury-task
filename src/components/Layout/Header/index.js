421180e2f7ddd58b4e71747c129b52084482c56e
gulshan <gulshan.s2108@gmail.com>
2022-07-29 12:43:02 +0530

    fixed design responsiveness

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -7,15 +7,15 @@ import {
     Button
 } from "react-bootstrap";
 import Logo from "../../../assets/fanfury-logo.svg";
-import {BsSearch} from "react-icons/bs";
 import { Link } from 'react-router-dom';
+import searchIcon from "../../../assets/search_ic.svg";
 
 const Header = () => {
   return (
     <>
         <Navbar expand="lg">
-            <Container>
-                <Link to="/">
+            <Container fluid>
+                <Link to="/" className="navbar-brand">
                     <img src={Logo} alt="logo" className='img-fluid' />
                 </Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -23,8 +23,8 @@ const Header = () => {
                 <Nav className="ms-auto align-items-center">
                     <Nav.Link className="active" href="/">Packages</Nav.Link>
                     <Nav.Link href="/">About Us</Nav.Link>
-                    <Nav.Link>
-                        <BsSearch />
+                    <Nav.Link className="search-link">
+                        <img src={searchIcon} alt="search_icon" />
                     </Nav.Link>
                     <Nav.Link>
                         <Button className='btn-header'>Play Now</Button>
--- src/components/PaymentIntegration/PaymentIntegration.js
+++ src/components/PaymentIntegration/PaymentIntegration.js
@@ -6,7 +6,7 @@ const PaymentIntegration = () => {
   return (
     <Col sm={5}>
         <div className='hero-content'>
-            <div className='hero-action-box'>
+            <div className='hero-action-box payment-box'>
                 <div className='bar-code-box'>
                     <img src={barCode} alt="bar-code" className='img-fluid' />
                 </div>
